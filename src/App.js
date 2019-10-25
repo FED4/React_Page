226cf24bea3403ccf6b8fee4b53ff499b48de2cb
YIYANG <feiyyfrank@gmail.com>
2019-10-25 16:26:43 +0100

    submit button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+/* eslint-disable */
 
 class App extends Component {
   render() {
-    return (
+  /*  return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +16,15 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>content</div>
+      </div>
+    );*/
+
+    return (
+      <div>
+      123
+        <label className="label" htmlFor="name">Enter name:</label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
       </div>
     );
   }
