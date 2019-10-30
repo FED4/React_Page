f5bf509e014ef8da939a64e93a40b78a3f7e8710
YIYANG <feiyyfrank@gmail.com>
2019-10-30 18:26:17 +0000

    color

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,57 +1,61 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css';
 
 class NavBar extends Component {
   render() {
     return (
 
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid summit-blue">
+          <div className="navbar-header summit-blue">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#">Summit</a>
           </div>
 
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+              <li><a href="news/">Link</a></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                <ul className="dropdown-menu">
                   <li><a href="#">Action</a></li>
                   <li><a href="#">Another action</a></li>
                   <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
+                  <li role="separator" className="divider"></li>
                   <li><a href="#">Separated link</a></li>
-                  <li role="separator" class="divider"></li>
+                  <li role="separator" className="divider"></li>
                   <li><a href="#">One more separated link</a></li>
                 </ul>
               </li>
             </ul>
-            <form class="navbar-form navbar-left">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search" />
+            <form className="navbar-form navbar-left">
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Search" />
               </div>
-              <button type="submit" class="btn btn-default">Submit</button>
+              <button type="submit" className="btn btn-default">Submit</button>
             </form>
-            <ul class="nav navbar-nav navbar-right">
+            <ul className="nav navbar-nav navbar-right summit-white">
+              <li><a href="news" className="summit-white">新闻</a></li>
               <li><a href="#">Link</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
+              <li><a href="#">Link</a></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                <ul className="dropdown-menu">
                   <li><a href="#">Action</a></li>
                   <li><a href="#">Another action</a></li>
                   <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
+                  <li role="separator" className="divider"></li>
                   <li><a href="#">Separated link</a></li>
                 </ul>
               </li>
+              <li><a href="#">Link</a></li>
             </ul>
           </div>
         </div>
